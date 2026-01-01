a33a8680aafa6476fe7870cb21d1f0e7cf2b67a6
build failed <krill.girdles4n@icloud.com>
2026-01-01 12:15:51 +0000

    Update and rename random_eye_pos to random_eye_pos.py

--- random_eye_pos.py
+++ random_eye_pos.py
@@ -3,6 +3,7 @@ from pythonosc import udp_client
 import random
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
+cooldown = 0.5
 
 while True:
     x,y,z = random.uniform(0, 360), random.uniform(0, 360),random.uniform(0, 360)
@@ -11,4 +12,4 @@ while True:
     pitch,yaw = random.uniform(0,360),random.uniform(-360,360)
     client.send_message("/tracking/eye/CenterPitchYaw", [pitch, yaw])
 
-    time.sleep(0.5)
+    time.sleep(cooldown)
